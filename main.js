ac92be0ae6e1a35884f2ef16fb4f3f0c6a375920
Fledson Henrique <fledsonhenrique@gmail.com>
2021-07-25 15:49:22 -0300

    concluido o projeto versão mobile e desktop

--- main.js
+++ main.js
@@ -16,9 +16,10 @@ for (const link of links) {
 }
 
 // ADICIONAR SHADOWS NO HEADER AO ROLAR PAGINA AO DAR SCROLL
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight
 
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
@@ -28,21 +29,15 @@ function changeHeaderWhenScroll() {
 }
 
 // BUTTOM BACK TO TOP
-function backToTop() {
-    const toTop = document.querySelector('.back-to-top')
+const toTop = document.querySelector('.back-to-top')
 
+function backToTop() {
     if (window.scrollY >= 560) {
         toTop.classList.add('show')
     } else {
         toTop.classList.remove('show')
     }
 }
-
-// WHEN SCROLL
-window.addEventListener('scroll', () => {
-    changeHeaderWhenScroll()
-    backToTop()
-})
 
 // TESTIMUNIALS CAROUSEL SLIDER SWIPER
 const swiper = new Swiper('.swiper-container', {
@@ -51,18 +46,25 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewhell: true,
-    keyboard: true
-        // effect: 'cube',
-        // grabCursor: true,
-        // cubeEffect: {
-        //     shadow: true,
-        //     slideShadows: true,
-        //     shadowOffset: 20,
-        //     shadowScale: 0.94
-        // },
-        // pagination: {
-        //     el: '.swiper-pagination'
-        // }
+    keyboard: true,
+    breakpoints: {
+        // menor ou igual a
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+    // effect: 'cube',
+    // grabCursor: true,
+    // cubeEffect: {
+    //     shadow: true,
+    //     slideShadows: true,
+    //     shadowOffset: 20,
+    //     shadowScale: 0.94
+    // },
+    // pagination: {
+    //     el: '.swiper-pagination'
+    // }
 })
 
 // SCROLLREVEAL -> MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PAGINA
@@ -83,3 +85,40 @@ scrollReveal.reveal(
     footer .brand, footer .social
 `, { interval: 90 }
 )
+
+// ATIVAR SEÇÃO NAVEGADA NO MENU
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+    // pegando o deslocamento da pagina + o tamnho da pagina dividido por 8 e dos 8 pegando apenas 4 pedaços
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const chepointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && chepointEnd) {
+            document
+                .querySelector(`nav ul li a[href*=${sectionId}]`)
+                .classList.add('active')
+        } else {
+            document
+                .querySelector(`nav ul li a[href*=${sectionId}]`)
+                .classList.remove('active')
+        }
+
+    }
+
+
+
+}
+// WHEN SCROLL
+window.addEventListener('scroll', () => {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
